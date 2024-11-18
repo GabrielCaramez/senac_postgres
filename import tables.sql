0f6ee704e6bf4d41458a9209618ea4b23aea480a
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2024-11-18 16:18:04 -0300

    import de tabelas para o postgres

---
+++ import tables.sql
@@ -0,0 +1,2 @@
+copy remuneracao201301 (nome, cpf, dc_empresa, dc_cargo, dc_funcao, dc_grupo_sitfunc, co_mes_referencia, co_exercicio, vl_liquido) from '/shared/banco de dados01.csv'
+DELIMITER ',' CSV HEADER;
